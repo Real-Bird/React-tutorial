2003030274e47b596229f3eab4b76e0125bafbb4
Real-Bird <marbil815@gmail.com>
2022-08-14 15:30:03 +0900

    22-08-14 what's redux?

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import AppRouter from "./Router";
+import CounterContainer from "./testRedux/CounterContainer";
 
 function App() {
   return (
     <>
-      <AppRouter />
+      <CounterContainer />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./index.css";
+import { createStore } from "redux";
+import counter from "./components/testRedux/modules/counter";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const store = createStore(counter, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
